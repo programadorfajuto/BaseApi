0f23f927231276ee66663604cdba0011c3385eeb
matheus santos <matheus.stag@gmail.com>
2019-07-09 10:23:52 -0300

    initial

---
+++ Api/Extensions/WebHostBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BaseApi.Api.Extensions
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UsePort(this IWebHostBuilder builder)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            return string.IsNullOrEmpty(port) ? builder : builder.UseUrls($"http://+:{port}");
+        }
+    }
+}
